04280cb8e08db6a5420e4e36e8ab7823b51861a2
adrian <adri.latorre@gmail.com>
2021-04-24 18:22:24 +0200

    Add sass and element

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,19 @@
 <template>
   <div>
+    <navbar />
     <Nuxt />
   </div>
 </template>
+
+<script>
+import Navbar from '@/components/common/navbar.vue'
+
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
 
 <style>
 html {
@@ -31,32 +42,5 @@ html {
   margin: 0;
 }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
 
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
 </style>
---
+++ plugins/element.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import { Button, Select } from 'element-ui';
+import '../assets/styles/element-variables.scss'
+
+Vue.use(Button)
+Vue.use(Select)
