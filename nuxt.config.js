f0ffbe855688b8e8a73a9a8c71d9d585837e077e
adrian <adri.latorre@gmail.com>
2021-04-25 08:48:48 +0200

    Update logo and name

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'carreers.io',
+    title: 'Career pages',
     htmlAttrs: {
       lang: 'en'
     },
